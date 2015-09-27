ee7fcfba2ba68b6f80101b3c5637292b2033b6f0
Saad <saad.18@gmail.com>
2015-09-27 21:50:23 +0530

    Add admin resume path

--- app/controllers/admin/resumes_controller.rb
+++ app/controllers/admin/resumes_controller.rb
@@ -28,6 +28,7 @@ class Admin::ResumesController < AdminController
   # PATCH/PUT /resumes/1
   # PATCH/PUT /resumes/1.json
   def update
+    @resume = Resume.joins(:user).find_by(users: {email: 'saad.18@gmail.com'})
     respond_to do |format|
       if @resume.update(resume_params)
         format.html { redirect_to @resume, notice: 'Resume was successfully updated.' }
