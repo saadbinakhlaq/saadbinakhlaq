200a825d5767d008b795d0cdeed0606f30ee34ab
Saad <saad.18@gmail.com>
2015-09-27 15:57:28 +0530

    Add admin controller and path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :resumes
   devise_for :users
   root to: 'home#index'
+
+  resources :resumes, only: [:show]
+
+  get '/resume' => 'resumes#show', as: :resume_page
+
+  namespace :admin do
+    resources :resumes
+  end
 end
